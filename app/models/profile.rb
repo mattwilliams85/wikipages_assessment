9e510c4e0fb1f5d30b4de703bb5b6881b5927a46
Matthew <mattjunk85@yahoo.com>
2014-08-31 13:17:23 -0700

    add validation for height numericality (passes)

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,7 @@ class Profile < ActiveRecord::Base
 	validates :name, :presence => :true
 	validates :content, :presence => :true
 	validates :height, :presence => :true
+	validates :height, :numericality => :true
 	validates :ethnicity, :presence => :true
 	validates :status, :presence => :true
 end
--- spec/profile_spec.rb
+++ spec/profile_spec.rb
@@ -6,4 +6,5 @@ describe Profile do
 	it { should validate_presence_of :height }
 	it { should validate_presence_of :ethnicity }
 	it { should validate_presence_of :status }
+	it { should validate_numericality_of :height }
 end
